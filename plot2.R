df05ab63983e2ac93307ea6cefa7c6a8cabe3ba0
Alfredo A Santana <aasantana@gmail.com>
2019-05-26 12:51:17 -0500

    Adds plot 2 files.

---
+++ plot2.R
@@ -0,0 +1,16 @@
+## plot2.R
+
+## Creates an line plot of the global active power based
+## on the Electric Power Consumption dataset for the period for a two day period
+## in February 2007.
+
+source("common.R")
+#Note: common.R defines loadData, which is reused in all the plot<X>.R scripts
+powerDF<-loadData()
+png("plot2.png", width=480, height=480)
+plot(powerDF$Global_active_power, type='l', xlab='', 
+     ylab="Global Active Power (kilowatts)", xaxt="n")
+firstFriday<-min(which(powerDF$Date=="2/2/2007"))
+axis(1, at=c(0,firstFriday, length(powerDF$Global_active_power)), 
+     labels=c('Thu', 'Fri', 'Sat'))
+dev.off()
