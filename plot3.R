35b27ff31393f99e760bac83aaff2d84ca2b445b
Alfredo A Santana <aasantana@gmail.com>
2019-05-26 13:25:34 -0500

    Adds comments

--- plot3.R
+++ plot3.R
@@ -8,13 +8,21 @@ source("common.R")
 #Note: common.R defines loadData, which is reused in all the plot<X>.R scripts
 powerDF<-loadData()
 png("plot3.png", width=480, height=480)
+
+# plot the first time series
 plot(powerDF$Sub_metering_1, type='l', xlab='', 
      ylab="Global Active Power (kilowatts)", xaxt="n")
+# plot the next time series
 lines(powerDF$Sub_metering_2, col="red")
+# plot the last time series
 lines(powerDF$Sub_metering_3, col="blue")
+
+# create the proper x axis changing the observation to the week day
 firstFriday<-min(which(powerDF$Date=="2/2/2007"))
 axis(1, at=c(0,firstFriday, length(powerDF$Global_active_power)), 
      labels=c('Thu', 'Fri', 'Sat'))
+
+# add a legend
 legTxt<-c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 legend("topright", legTxt, col = c("black", "red", "blue"), lty=c(1,1,1))
 dev.off()
